a2f990575044957028e65e9865d18dc9db46c24a
Uchiha-Satori <stephen.kit.ho@gmail.com>
2017-08-29 23:26:14 -0400

    Project 2 Update
    Changed apps.js address to back end server.

--- assets/scripts/auth/app.js
+++ assets/scripts/auth/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
 module.exports = {
-  host: 'http://localhost:4741/'
-  // add heroku address here and then comment out local host.
+  // host: 'http://localhost:4741/'
+  host: 'https://asiannerdist.herokuapp.com/'
 }
